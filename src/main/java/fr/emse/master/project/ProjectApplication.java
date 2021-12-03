67dc881faa797cb0018a1320ab033ab56ea08b0f
Ahmad AYADA <mr.ayadi@gmail.com>
2021-12-03 22:27:55 +0100

    Add Plateforme_Territoire files

--- src/main/java/fr/emse/master/project/ProjectApplication.java
+++ src/main/java/fr/emse/master/project/ProjectApplication.java
@@ -1,5 +1,10 @@
 package fr.emse.master.project;
 
+import java.io.InputStream;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,16 @@ public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+		System.err.println("Hellow there \n");
+		// System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		// Reading RDF
+		// create an empty model
+		Model model = ModelFactory.createDefaultModel();
+		// use the RDFDataMgr to find the input file
+		String inputFileName = "./Plateforme_Territoire/ontology.rdf";
+		InputStream in = RDFDataMgr.open(inputFileName);
+
+		
 	}
 
 }
