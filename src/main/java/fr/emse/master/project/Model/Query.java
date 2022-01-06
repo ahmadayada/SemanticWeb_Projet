fe22703c0ac7a1f297d65b985defcf75a184ed45
Ahmad AYADA <mr.ayadi@gmail.com>
2022-01-07 00:13:16 +0100

    Add Ibtissam Works Files

--- src/main/java/fr/emse/master/project/Model/Query.java
+++ src/main/java/fr/emse/master/project/Model/Query.java
@@ -16,17 +16,23 @@ public class Query {
         String datasetURL = "http://localhost:3030/ProjetDataSet";
         String sparqlEndpoint = datasetURL + "/sparql";
         String sparqlQuery = datasetURL + "/query";
-        String graphStore = datasetURL + "/data";
-        try {
-            RDFConnection conneg = RDFConnectionFactory.connect(sparqlEndpoint, sparqlQuery, graphstore);
-            QueryExecution qExec = conneg.query("prefix bot: <https://w3id. org/bot#>" +
-                    "prefix dul: <http://www. ontologydesignpatterns .org/ont/dul/DUL .owl#>" +
-                    "prefix sosa: <http://www.w3. org/ns/sosa/>" +
-                    " SELECT DISTINCT ?s { ?s a bot : Space . ?se a sosa: Sensor; dul: hasLocation ?s. }");
-            ResultSet rs = qExec.execSelect();
-
-        } catch (Exception e) {
-
-        }
+        String graphStore = datasetURL + "/data";/*
+                                                  * try {
+                                                  * // RDFConnection conneg =
+                                                  * RDFConnectionFactory.connect(sparqlEndpoint,
+                                                  * // sparqlQuery, graphstore);
+                                                  * QueryExecution qExec =
+                                                  * conneg.query("prefix bot: <https://w3id. org/bot#>" +
+                                                  * "prefix dul: <http://www. ontologydesignpatterns .org/ont/dul/DUL .owl#>"
+                                                  * +
+                                                  * "prefix sosa: <http://www.w3. org/ns/sosa/>" +
+                                                  * " SELECT DISTINCT ?s { ?s a bot : Space . ?se a sosa: Sensor; dul: hasLocation ?s. }"
+                                                  * );
+                                                  * ResultSet rs = qExec.execSelect();
+                                                  * 
+                                                  * } catch (Exception e) {
+                                                  * 
+                                                  * }
+                                                  */
     }
 }
