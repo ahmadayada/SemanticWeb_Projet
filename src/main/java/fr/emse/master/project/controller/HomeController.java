f6c786203c3441b319ca49fd3bd5489a1a2073fe
Ahmad AYADA <mr.ayadi@gmail.com>
2022-01-06 23:26:08 +0100

    fix bug

--- src/main/java/fr/emse/master/project/controller/HomeController.java
+++ src/main/java/fr/emse/master/project/controller/HomeController.java
@@ -1,4 +1,4 @@
-package fr.emse.master.project.Controller;
+package fr.emse.master.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
